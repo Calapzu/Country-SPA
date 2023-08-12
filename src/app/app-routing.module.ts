c30c82202a5a39877633e0a2432f1761ff3e3881
Calapzu <yamsoncalapzu@gmail.com>
2023-08-12 11:41:31 -0500

    RouterLink y RouterLinkOptions

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,15 +3,20 @@ import { RouterModule, Routes } from '@angular/router';
 
 import { HomePageComponent } from './shared/Pages/home-page/home-page.component';
 import { AboutPageComponent } from './shared/Pages/about-page/about-page.component';
+import { ContactPageComponent } from './shared/Pages/contact-page/contact-page.component';
 
 const routes: Routes =[
   {
-    path: 'home',
+    path: '',
     component: HomePageComponent
   },
   {
     path: 'about',
     component: AboutPageComponent
+  },
+  {
+    path: 'contact',
+    component: ContactPageComponent
   },
   {
     path: '**',
--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,22 +1,32 @@
+import { RouterModule } from '@angular/router';
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+
 import { HomePageComponent } from './Pages/home-page/home-page.component';
 import { AboutPageComponent } from './Pages/about-page/about-page.component';
+import { SidebarComponent } from './components/sidebar/sidebar.component';
+import { ContactPageComponent } from './Pages/contact-page/contact-page.component';
 
 
 
 @NgModule({
   declarations: [
     AboutPageComponent,
+    ContactPageComponent,
     HomePageComponent,
+    SidebarComponent,
+
 
   ],
   imports: [
-    CommonModule
+    CommonModule,
+    RouterModule
   ],
   exports: [
     AboutPageComponent,
+    ContactPageComponent,
     HomePageComponent,
+    SidebarComponent
   ]
 })
 export class SharedModule { }
