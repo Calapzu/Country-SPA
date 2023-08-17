a926be5b27e5d336a86d1ef0dd8ae35d0230c098
Calapzu <yamsoncalapzu@gmail.com>
2023-08-17 18:06:57 -0500

    Refactorizaciones en nuestro servicio

--- src/app/countries/services/countries.service.ts
+++ src/app/countries/services/countries.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { Observable, catchError, map, of, tap } from 'rxjs';
+import { Observable, catchError, delay, map, of, tap } from 'rxjs';
 import { Country } from '../interfaces/country';
 
 
@@ -9,6 +9,14 @@ export class CountriesService {
   private apiUrl: string = 'https://restcountries.com/v3.1';
 
   constructor(private http: HttpClient) {}
+
+  private getCountriesRequest( url: string): Observable<Country[]>{
+    return this.http.get<Country[]>( url )
+    .pipe(
+      catchError( () => of([])),
+      delay( 2000 )
+    );
+  }
 
   searchCountryByAlphaCode( code: string ): Observable<Country | null> {
     const url = `${this.apiUrl}/alpha/${code}`;
@@ -23,7 +31,7 @@ export class CountriesService {
   searchCapital(term: string): Observable<Country[]> {
     const url = `${this.apiUrl}/capital/${term}`;
 
-    return this.http.get<Country[]>(url).pipe(
+    /*return this.http.get<Country[]>(url).pipe(
       // tap( countries => console.log('Paso por el tap 1', countries)),
       // map( countries => [] ), //El operador map transforma la informacion
       // tap( countries => console.log('Paso por el tap 2', countries))
@@ -32,18 +40,19 @@ export class CountriesService {
       //   return of([]);
       // })
       catchError(() => of([]))
-    );
+    );*/
+    return this.getCountriesRequest(url);
   }
 
   searchCountry(term: string): Observable<Country[]> {
     const url = `${this.apiUrl}/name/${term}`;
 
-    return this.http.get<Country[]>(url).pipe(catchError(() => of([])));
+    return this.getCountriesRequest(url);
   }
 
   searchRegion(term: string): Observable<Country[]> {
     const url = `${this.apiUrl}/region/${term}`;
 
-    return this.http.get<Country[]>(url).pipe(catchError(() => of([])))
+    return this.getCountriesRequest(url);
   }
 }
